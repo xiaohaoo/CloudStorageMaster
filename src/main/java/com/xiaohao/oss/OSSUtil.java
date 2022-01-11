8190d17fd9850919b9eeebd8d0a238f0ac8c678d
小浩 <sdwenhappy@163.com>
2022-01-11 19:40:46 +0800

    上传文件

--- src/main/java/com/xiaohao/oss/OSSUtil.java
+++ src/main/java/com/xiaohao/oss/OSSUtil.java
@@ -24,22 +24,42 @@ public class OSSUtil {
         ACCESS_KEY_ID = accessKeyId;
         ACCESS_KEY_SECRET = accessKeySecret;
         END_POINT = endpoint;
-        END_POINT_PARSE = END_POINT.replace("https://", "");
+        END_POINT_PARSE = END_POINT.replaceAll("http[s]?://", "");
     }
 
     /**
      * 上传
      *
-     * @param bucketName
-     * @param key
-     * @param inputStream
-     * @return
+     * @param bucketName  存储桶
+     * @param key         文件路径
+     * @param inputStream 输入流
+     * @return 文件Http访问地址
      */
     public static String upload(String bucketName, String key, InputStream inputStream) {
         OSS ossClient = new OSSClientBuilder().build(END_POINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
-        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, inputStream);
-        ossClient.putObject(putObjectRequest);
-        ossClient.shutdown();
-        return String.format("https://%s.%s", bucketName, END_POINT_PARSE);
+        try {
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, key, inputStream);
+            ossClient.putObject(putObjectRequest);
+            return String.format("https://%s.%s/%s", bucketName, END_POINT_PARSE, key);
+        } finally {
+            ossClient.shutdown();
+        }
+    }
+
+
+    /**
+     * 判断文件是否存在
+     *
+     * @param bucketName 存储桶
+     * @param key        文件路径
+     * @return 是否存在
+     */
+    public static boolean isExist(String bucketName, String key) {
+        OSS ossClient = new OSSClientBuilder().build(END_POINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+        try {
+            return ossClient.doesObjectExist(bucketName, key);
+        } finally {
+            ossClient.shutdown();
+        }
     }
 }
